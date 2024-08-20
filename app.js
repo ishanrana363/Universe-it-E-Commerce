aedc7012c223350a20bd97119f5d1c4cd07966a5
ishanrana363 <ishanrana363@gmail.com>
2024-08-20 15:10:52 +0600

    modify home route

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(limiter)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({limit: '50mb'}));
 
-app.get("/",()=>{
+app.get("/",async(req,res)=>{
     res.send("Server is running");
 })
 
